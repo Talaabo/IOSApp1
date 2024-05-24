16cb61a114d62395dfb3d5409b91cc617d3c17b0
Mustafe Ahmed <160895798+Talaabo@users.noreply.github.com>
2024-05-24 10:09:22 -0400

    initial IOSApp1

---
+++ IOSApp1/IOSApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSApp1
+//
+//  Created by Owner on 2024-05-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
