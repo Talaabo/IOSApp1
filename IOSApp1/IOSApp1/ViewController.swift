bc09899f002bc4aac50ec45a7817c7bfc86206da
Mustafe Ahmed <160895798+Talaabo@users.noreply.github.com>
2024-06-02 22:50:49 -0400

    end of the app

--- IOSApp1/IOSApp1/ViewController.swift
+++ IOSApp1/IOSApp1/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-  
+
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         return imageView
     }()
     
-    private let button: UIButton = {
+    private let randomButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .white
         button.setTitle("Random Photo", for: .normal)
@@ -24,34 +24,248 @@ class ViewController: UIViewController {
         return button
     }()
     
+    private let nextButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.setTitle("Next Photo", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    private let prevButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.setTitle("Previous Photo", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    private let slideshowButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemGreen
+        button.setTitle("Start Slideshow", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    private let photoIndexLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Photo 1"
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    
+    private let slideshowStatusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Slideshow: Stopped"
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    
+    private var photoURLs: [String] = []
+    private var currentIndex: Int = 0
+    private var slideshowTimer: Timer?
+    private var isSlideshowRunning = false
+    
     private let colors: [UIColor] = [
-        .systemRed, .systemGreen, .systemBlue, .systemOrange, .systemYellow, .systemPurple, .systemPink
+        .systemRed, .systemGreen, .systemCyan, .systemOrange, .systemYellow, .systemPurple, .systemPink
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemCyan
+        
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
-        view.addSubview(button)
-        getRandomPhoto()
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
+        view.addSubview(randomButton)
+        view.addSubview(nextButton)
+        view.addSubview(prevButton)
+        view.addSubview(slideshowButton)
+        view.addSubview(photoIndexLabel)
+        view.addSubview(slideshowStatusLabel)
+        
+        fetchPhotoURLs {
+            self.updatePhoto()
+        }
+        
+        randomButton.addTarget(self, action: #selector(didTapRandomButton), for: .touchUpInside)
+        nextButton.addTarget(self, action: #selector(didTapNextButton), for: .touchUpInside)
+        prevButton.addTarget(self, action: #selector(didTapPrevButton), for: .touchUpInside)
+        slideshowButton.addTarget(self, action: #selector(didTapSlideshowButton), for: .touchUpInside)
     }
     
-    @objc func didTapButton() {
+    @objc func didTapRandomButton() {
         getRandomPhoto()
         view.backgroundColor = colors.randomElement()
     }
     
+    @objc func didTapNextButton() {
+        guard !photoURLs.isEmpty else { return }
+        currentIndex = (currentIndex + 1) % photoURLs.count
+        updatePhoto()
+    }
+    
+    @objc func didTapPrevButton() {
+        guard !photoURLs.isEmpty else { return }
+        currentIndex = (currentIndex - 1 + photoURLs.count) % photoURLs.count
+        updatePhoto()
+    }
+    
+    @objc func didTapSlideshowButton() {
+        if isSlideshowRunning {
+            stopSlideshow()
+        } else {
+            startSlideshow()
+        }
+    }
+    
+    private func startSlideshow() {
+        slideshowTimer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(showNextPhoto), userInfo: nil, repeats: true)
+        slideshowStatusLabel.text = "Slideshow: Running"
+        slideshowButton.setTitle("Stop Slideshow", for: .normal)
+        isSlideshowRunning = true
+    }
+    
+    private func stopSlideshow() {
+        slideshowTimer?.invalidate()
+        slideshowTimer = nil
+        slideshowStatusLabel.text = "Slideshow: Stopped"
+        slideshowButton.setTitle("Start Slideshow", for: .normal)
+        isSlideshowRunning = false
+    }
+    
+    @objc private func showNextPhoto() {
+        guard !photoURLs.isEmpty else { return }
+        currentIndex = (currentIndex + 1) % photoURLs.count
+        updatePhoto()
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        button.frame = CGRect(
+        randomButton.frame = CGRect(
             x: 30,
-            y: view.frame.size.height - 150 - view.safeAreaInsets.bottom,
+            y: view.frame.size.height - 250 - view.safeAreaInsets.bottom,
             width: view.frame.size.width - 60,
             height: 55
         )
+        
+        nextButton.frame = CGRect(
+            x: 30,
+            y: randomButton.frame.maxY + 20,
+            width: (view.frame.size.width - 90) / 2,
+            height: 55
+        )
+        
+        prevButton.frame = CGRect(
+            x: nextButton.frame.maxX + 30,
+            y: randomButton.frame.maxY + 20,
+            width: (view.frame.size.width - 90) / 2,
+            height: 55
+        )
+        
+        slideshowButton.frame = CGRect(
+            x: 30,
+            y: nextButton.frame.maxY + 20,
+            width: view.frame.size.width - 60,
+            height: 55
+        )
+        
+        photoIndexLabel.frame = CGRect(
+            x: 30,
+            y: slideshowButton.frame.maxY + 20,
+            width: view.frame.size.width - 60,
+            height: 45
+        )
+        
+        slideshowStatusLabel.frame = CGRect(
+            x: 30,
+            y: photoIndexLabel.frame.maxY + 10,
+            width: view.frame.size.width - 60,
+            height: 45
+        )
+    }
+    
+    private func updatePhoto() {
+        guard !photoURLs.isEmpty else { return }
+        let urlString = photoURLs[currentIndex]
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Failed to fetch image data: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("Invalid response")
+                return
+            }
+            
+            print("HTTP Response Status Code: \(httpResponse.statusCode)")
+            
+            guard httpResponse.statusCode == 200 else {
+                print("HTTP Error: \(httpResponse.statusCode)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data fetched")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if let image = UIImage(data: data) {
+                    self.imageView.image = image
+                    self.photoIndexLabel.text = "Photo \(self.currentIndex + 1)"
+                } else {
+                    print("Failed to create image from data")
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    private func fetchPhotoURLs(completion: @escaping () -> Void) {
+        let urlString = "https://picsum.photos/v2/list"
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Failed to fetch photo URLs: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data fetched")
+                return
+            }
+            
+            do {
+                if let jsonArray = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] {
+                    for json in jsonArray {
+                        if let urlString = json["download_url"] as? String {
+                            self.photoURLs.append(urlString)
+                        }
+                    }
+                    DispatchQueue.main.async {
+                        completion()
+                    }
+                }
+            } catch {
+                print("Failed to parse JSON: \(error.localizedDescription)")
+            }
+        }
+        task.resume()
     }
     
     func getRandomPhoto() {
@@ -95,3 +309,4 @@ class ViewController: UIViewController {
         task.resume()
     }
 }
+
